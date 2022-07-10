acf1a37e00a0d3f3c0541f6b5b7b374aa97ac3be
Saoa35 <saoamah@gmail.com>
2022-07-10 21:36:45 +0200

    irebase

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9HqG6NteqYoqrcz46rOxBWdhwSc1UJD4",
+    authDomain: "food-rest-d52ab.firebaseapp.com",
+    databaseURL: "https://food-rest-d52ab-default-rtdb.firebaseio.com",
+    projectId: "food-rest-d52ab",
+    storageBucket: "food-rest-d52ab.appspot.com",
+    messagingSenderId: "146693459983",
+    appId: "1:146693459983:web:89da6077bbf1e20204537a"
+  };
