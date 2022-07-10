1c6c0f9fe069b3ed2d34d7beaac4e5b28b61ded8
Saoa35 <saoamah@gmail.com>
2022-07-10 22:32:32 +0200

    comment import

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyC9HqG6NteqYoqrcz46rOxBWdhwSc1UJD4",
     authDomain: "food-rest-d52ab.firebaseapp.com",
@@ -7,3 +9,5 @@ const firebaseConfig = {
     messagingSenderId: "146693459983",
     appId: "1:146693459983:web:89da6077bbf1e20204537a"
   };
+
+  firebase.initializeApp(firebaseConfig);
