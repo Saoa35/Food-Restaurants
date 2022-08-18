bfd5a8107e886bcc30e42d7e4a9816f64dd37ce2
Saoa35 <saoamah@gmail.com>
2022-08-18 23:07:35 +0400

    created watching func

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const scss = require('gulp-sass')(require('sass'));
 
 function styles() {
     return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({outputStyle: 'expanded'}))
     .pipe(dest('app/css'));
 }
 
+function watching() {
+    watch(['app/scss/**/*.scss'], styles);
+}
+
 exports.styles = styles;
+exports.watching = watching;
