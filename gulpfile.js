441d7ebc358f02992a430693a3ec760bf4938cff
Saoa35 <saoamah@gmail.com>
2022-08-19 12:46:07 +0400

    + gulp-uglify-es

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const scss = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 
@@ -13,12 +13,17 @@ function browsersync() {
 function styles() {
     return src('app/scss/style.scss')
     .pipe(scss({outputStyle: 'expanded'}))
-    .pipe(dest('app/css'));
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
+    watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
 exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(browsersync, watching);
